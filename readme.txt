5c14cb112ed2b65e3ab392627545691b565c773d
matsuoshi <matsuoshi@gmail.com>
2013-12-28 13:57:54 +0900

    first commit

---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Small Archives ===
+Contributors: h.matsuo
+Tags: archive, archives, widget
+Requires at least: 3.0
+Tested up to: 3.8
+Stable tag: 1.0
+License: GPLv2
+
+Small Archives is monthly archive widget, display smaller list.
+
+== Description ==
+
+Small Archives is monthly archive widget, display smaller list.
+
+== Installation ==
+
+1. Upload the `small-archives` folder to the `/wp-content/plugins/` directory.
+
+2. Activate the plugin through the Plugins menu in WordPress.
+
+== Screenshots ==
+
+1. Small Archives sample.
+
+== Changelog ==
+= 1.0 =
+* 2013.12 first version.
---
+++ small-archives.php
@@ -0,0 +1,187 @@
+<?php
+/*
+Plugin Name: Small Archives
+Description: monthly archive widget, display smaller list.
+Version: 1.0
+Plugin URI: https://github.com/matsuoshi/wp-small-archives
+Author: h.matsuo
+Author URI: https://github.com/matsuoshi/
+License: GPLv2
+*/
+
+/**
+ * Small Archives Widget class
+ */
+class SmallArchivesWidget extends WP_Widget
+{
+	/**
+	 * constructor
+	 */
+	function __construct()
+	{
+		add_action('get_header', array($this, 'addCss'));
+
+		parent::__construct(false, 'Small Archives');
+	}
+
+
+	/**
+	 * widget main
+	 * @param array $args
+	 * @param array $instance
+	 */
+	function widget($args, $instance)
+	{
+		extract($args);
+
+		echo $before_widget;
+
+		$title = ($instance['title']) ? : __('Archives', $this->text_domain);
+		echo $before_title . apply_filters('widget_title', $title) . $after_title;
+
+		$archives = $this->getArchives($instance);
+		if ($archives) {
+			if (! empty($instance['reverseYearOrder'])) {
+				krsort($archives);
+			}
+?>
+			<ul class="smallArchivesYearList">
+			<?php
+			foreach ($archives as $year => $a) :
+				if (! empty($instance['reverseMonthOrder'])) {
+					krsort($a);
+				}
+				$year_url = get_year_link($year);
+			?>
+				<li>
+					<span><a href="<?php echo $year_url ?>"><?php echo $year ?></a></span>
+					<ul class="smallArchivesMonthList">
+					<?php
+					foreach ($a as $month => $count) :
+
+						$month_after = ($instance['showPostCount']) ? "<span>({$count})</span>" : '';
+						$month_link = get_archives_link(get_month_link($year, $month), $month, $format, $before, $month_after . $after);
+					?>
+						<li><?php echo $month_link ?></li>
+					<?php endforeach; ?>
+					</ul>
+				</li>
+			<?php endforeach; ?>
+			</ul>
+<?php
+		}
+
+		echo $after_widget . "\n";
+	}
+
+
+	/**
+	 * call query
+	 * @param $instance
+	 * @return array
+	 */
+	function getArchives($instance)
+	{
+		global $wpdb;
+
+		// create query
+		$query = "
+			SELECT
+				YEAR(post_date) AS `year`,
+				MONTH(post_date) AS `month`,
+				count(ID) as posts
+			FROM
+				{$wpdb->posts}
+			WHERE
+				post_type = 'post' AND
+				post_status = 'publish'
+			GROUP BY
+				YEAR(post_date), MONTH(post_date)
+			ORDER BY
+				post_date ASC
+		";
+
+		// query
+		$results = $wpdb->get_results($query);
+
+		// format result
+		$archives = array();
+		foreach ($results as $result) {
+			$archives[$result->year][$result->month] = $result->posts;
+		}
+
+		return $archives;
+	}
+
+
+	/**
+	 * update widget (admin page)
+	 * @param array $new_instance
+	 * @param array $old_instance
+	 * @return array
+	 */
+	function update($new_instance, $old_instance)
+	{
+		$instance = $old_instance;
+		$new_instance = wp_parse_args((array)$new_instance, array(
+			'title' => '',
+			'reverseYearOrder' => false,
+			'reverseMonthOrder' => false,
+			'showPostCount' => false,
+		));
+
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['reverseYearOrder'] = (boolean)$new_instance['reverseYearOrder'];
+		$instance['reverseMonthOrder'] = (boolean)$new_instance['reverseMonthOrder'];
+		$instance['showPostCount'] = (boolean)$new_instance['showPostCount'];
+
+		return $instance;
+	}
+
+
+	/**
+	 * config widget (admin page)
+	 * @param array $instance
+	 * @return string|void
+	 */
+	function form($instance)
+	{
+		$instance = wp_parse_args((array)$instance, array(
+			'title' => '',
+			'reverseYearOrder' => true,
+			'reverseMonthOrder' => false,
+			'showPostCount' => false,
+		));
+		$title = strip_tags($instance['title']);
+		$reverseYearOrder = $instance['reverseYearOrder'] ? 'checked="checked"' : '';
+		$reverseMonthOrder = $instance['reverseMonthOrder'] ? 'checked="checked"' : '';
+		$showPostCount = $instance['showPostCount'] ? 'checked="checked"' : '';
+?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>">
+				<?php _e('Title:', $this->text_domain); ?>
+			</label>
+			<input type="text" id="<?php echo $this->get_field_id('title'); ?>" class="widefat" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo esc_attr($title); ?>" />
+		</p>
+		<p>
+			<input class="checkbox" type="checkbox" <?php echo $reverseYearOrder; ?> id="<?php echo $this->get_field_id('reverseYearOrder'); ?>" name="<?php echo $this->get_field_name('reverseYearOrder'); ?>" /> <label for="<?php echo $this->get_field_id('reverseYearOrder'); ?>"><?php _e('Reverse year order', $this->text_domain); ?></label>
+			<br/>
+			<input class="checkbox" type="checkbox" <?php echo $reverseMonthOrder; ?> id="<?php echo $this->get_field_id('reverseMonthOrder'); ?>" name="<?php echo $this->get_field_name('reverseMonthOrder'); ?>" /> <label for="<?php echo $this->get_field_id('reverseMonthOrder'); ?>"><?php _e('Reverse month order', $this->text_domain); ?></label>
+			<br/>
+			<input class="checkbox" type="checkbox" <?php echo $showPostCount; ?> id="<?php echo $this->get_field_id('showPostCount'); ?>" name="<?php echo $this->get_field_name('showPostCount'); ?>" /> <label for="<?php echo $this->get_field_id('showPostCount'); ?>"><?php _e('Show post counts', $this->text_domain); ?></label>
+		</p>
+<?php
+	}
+
+
+	/**
+	 * add css file
+	 */
+	function addCss()
+	{
+		wp_enqueue_style('SmallArchives', plugins_url('small-archives.css', __FILE__), false);
+	}
+}
+
+
+add_action('widgets_init', create_function('', 'return register_widget("SmallArchivesWidget");'));
